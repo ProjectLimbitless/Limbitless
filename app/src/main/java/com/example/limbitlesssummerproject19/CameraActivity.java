1ff47786a476e40625ffba262c997b0d0ef50ced
Eric <engo1207@gmail.com>
2019-07-15 23:50:31 -0700

    testing...

--- app/src/main/java/com/example/limbitlesssummerproject19/CameraActivity.java
+++ app/src/main/java/com/example/limbitlesssummerproject19/CameraActivity.java
@@ -9,5 +9,6 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        System.out.println("hello");
     }
 }
