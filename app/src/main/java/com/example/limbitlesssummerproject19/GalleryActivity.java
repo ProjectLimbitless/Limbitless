2c58c4d6843438114b401d7702ac509ec79a79dc
NiyonikaS <njsheth@ucsd.edu>
2019-07-17 22:44:49 -0700

    display thumbnails for each session

--- app/src/main/java/com/example/limbitlesssummerproject19/GalleryActivity.java
+++ app/src/main/java/com/example/limbitlesssummerproject19/GalleryActivity.java
@@ -1,5 +1,4 @@
 package com.example.limbitlesssummerproject19;
-//package com.androstock.galleryapp;
 
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -11,8 +10,12 @@ import android.graphics.drawable.Drawable;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -29,13 +32,10 @@ import java.util.Locale;
 public class GalleryActivity extends AppCompatActivity {
 
     static final int REQUEST_PERMISSION_KEY = 1;
-    //LoadAlbum loadAlbumTast;
     GridView sessionGallery;
 
-    // List containing paths to each session (directory)
-    ArrayList<HashMap<String, String>> albumList = new ArrayList<HashMap<String, String>>();
-
-    private String[] sessionThumbnails = null;
+    // List containing image thumbnails for each session folder
+    private ArrayList<Bitmap> sessionThumbnails = new ArrayList<Bitmap>();
 
 
     @Override
@@ -44,7 +44,7 @@ public class GalleryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gallery);
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
 
-
+        // Open ProstheticFolder directory
         String directoryName = Environment.getExternalStorageDirectory()+File.separator+"ProstheticFolder";
         File countFiles = new File(directoryName);
         File[] files = countFiles.listFiles(new FileFilter() {
@@ -53,116 +53,59 @@ public class GalleryActivity extends AppCompatActivity {
                 return pathname.isDirectory();
             }
         });
-        System.out.println("In gallery activity" + Arrays.toString(files));
 
-        // Get session thumbnails
+        // Get session thumbnails  (image at first index of each session)
         for ( File f : files ) {
-            System.out.println("curr file " + f.toString());
-            System.out.println("contents " + Arrays.toString(f.listFiles()));
-
+            File[] insideFile = f.listFiles();
+            if(insideFile.length != 0){
+                sessionThumbnails.add(BitmapFactory.decodeFile(insideFile[0].getAbsolutePath()));
+            }
         }
-        //sessionGallery.setAdapter(new ArrayAdapter<String>(cw, Arrays.toString(files)));
+
+        // Use adapter class as data provider
+        sessionGallery = (GridView)findViewById(R.id.galleryGridView);
+        GalleryAdapter galleryAdapter = new GalleryAdapter(this, sessionThumbnails);
+        sessionGallery.setAdapter(galleryAdapter);
     }
 
-    /** displayAlbum
-     *  Display all sessions in the gallery
-     *  
-     */
-
-    /** displayImages
-     *  Display all images in user selected session
-     */
 
     /**
-      * saveImage
-      * Temporary method to save dummy images to internal storage
+     * GalleryAdapter
+     * Data provider for gallery gridView
+     */
+    public class GalleryAdapter extends BaseAdapter {
 
-    private void saveImage(int id) {
+        private final Context mContext;
+        private final ArrayList<Bitmap> thumbnails;
 
-        // FOR INTERNAL STORAGE
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        File dir = cw.getDir("Limbitless", Context.MODE_PRIVATE);
-        File myImage = new File(dir, "image1");
-
-        // Get the image from drawable resource as drawable object
-        Drawable drawable = getDrawable(id);
-
-        // Get the bitmap from drawable object
-        Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
-
-        OutputStream os = null;
-
-        try {
-            os = new FileOutputStream(myImage);
-            bitmap.compress(Bitmap.CompressFormat.JPEG,100,os);
-            os.flush();
-            os.close();
-            System.out.println("Image Saved Successfully " + id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
-
-        /*
-        File imageDirectory;
-        String storageState = Environment.getExternalStorageState();
-        if (storageState.equals(Environment.MEDIA_MOUNTED)) {
-            imageDirectory = new File(
-                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                    "ProstheticFolder");
-            if (!imageDirectory.exists() && !imageDirectory.mkdirs()) {
-                imageDirectory = null;
-            } else {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_mm_dd_hh_mm",
-                        Locale.getDefault());
-
-                return new File(imageDirectory.getPath() +
-                        File.separator + "image_" +
-                        dateFormat.format(new Date()) + ".png");
-            }
-        }
-        return null;
-    }*/
-
-
-
-
-
-
-
-    /*
-     * SAVES THE BITMAP DATA OF THE PICTURE INTO A FILE USING THE PATH FROM openFileForImage
-     * FUNCTION. THIS COMPLETES SAVING THE IMAGE INTO THE PHONE.
-
-
-    private void saveToInternalStorage() {
-
-        File mypath = openFileForImage();
-        FileOutputStream fos;
-
-        try {
-            fos = new FileOutputStream(mypath);
-
-            Bitmap bmp = BitmapFactory.decodeByteArray(mCameraData, 0,
-                    mCameraData.length);
-            Matrix matrix = new Matrix();
-            if (android.os.Build.VERSION.SDK_INT < 9) {
-                matrix.postRotate(90);
-            } else {
-               // int orientation = determineDisplayOrientation();
-                //matrix.postRotate(orientation);
-            }
-            bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(),
-                    bmp.getHeight(), matrix, true);
-            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-            fos.flush();
-            fos.close();
-            Toast.makeText(getApplicationContext(), "Image was saved!",
-                    Toast.LENGTH_LONG).show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        // Constructor
+        public GalleryAdapter(Context context, ArrayList<Bitmap> src){
+            this.mContext = context;
+            this.thumbnails = src;
         }
 
-    }*/
+        @Override
+        public int getCount(){
+            return thumbnails.size();
+        }
+
+        @Override
+        public long getItemId(int position){
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position){
+            return null;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView iv = new ImageView(mContext);
+            iv.setImageBitmap(thumbnails.get(position));
+            return iv;
+        }
+    }
+
+
 }
