467b6e598c2f98e41b5ebfcb456937e6bb6963dd
sebastiantroncoso <sctroncoso10@gmail.com>
2020-02-09 22:51:45 -0800

    Adding the layout of the tutorial, need to learn how to add beautiful images

--- app/src/main/java/com/example/limbitlesssummerproject19/TutorialActivity.java
+++ app/src/main/java/com/example/limbitlesssummerproject19/TutorialActivity.java
@@ -1,51 +1,37 @@
 package com.example.limbitlesssummerproject19;
 
-import android.support.v4.app.Fragment;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 
 // Creating a fragment instead of an activity
 public class TutorialActivity extends FragmentActivity {
 
     private static final int NUM_PAGES = 5;
-    private ViewPager mPager;
-    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
 
-
         // What the hell is going on with mPager
-        mPager = (ViewPager) findViewById(R.id.pager);
-        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        ViewPager mPager = findViewById(R.id.pager);
+        PagerAdapter pagerAdapter = new ImageAdapter(this);
         mPager.setPageTransformer(true, new ZoomOutPageTransformer());
         mPager.setAdapter(pagerAdapter);
     }
 
-    // Simple adapter that represents 5 ScreenSlidePageFragment objects
-
-    private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager supportFragmentManager) {
-            super(supportFragmentManager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return new TutorialFragment();
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
